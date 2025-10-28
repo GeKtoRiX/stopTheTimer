0196ac0b5aa06704db1c082fe940b190abb5168e
GeKtoRiX <gektor93@gmail.com>
2025-10-28 14:46:05 +0300

    added useState simple logic with Player name

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,10 +1,29 @@
+import { useState } from 'react';
+
 export default function Player() {
+  // Хук отслеживания состояния имени игрока(String).
+  const [enteredPlayerName, setEnteredPlayerName] = useState('');
+  function handlePlayerName(event) {
+    setSubmitted(false);
+    // Отслеживания и изменение строки имени пользователя.
+    setEnteredPlayerName(event.target.value);
+  }
+  // Хук отслеживания состояния нажатия кнопки Submitted(False).
+  const [submitted, setSubmitted] = useState(false);
+  function hadnleClickSubmitted() {
+    // Отслеживание и изменение состоянки кнопки Submitted(Set Name).
+    setSubmitted(true);
+  }
   return (
     <section id='player'>
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
       <p>
-        <input type='text' />
-        <button>Set Name</button>
+        <input
+          type='text'
+          value={enteredPlayerName}
+          onChange={handlePlayerName}
+        />
+        <button onClick={hadnleClickSubmitted}>Set Name</button>
       </p>
     </section>
   );
